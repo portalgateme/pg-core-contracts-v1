86540867cb47cf29253489e05f7a8c6ff256c0c7
cryptoleek-eth <cryptoleek.eth@gmail.com>
2023-05-22 13:15:38 +1000

    update pgrouter and add instance registry

---
+++ contracts/portalgate/InstanceRegistry.sol
@@ -0,0 +1,192 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../interfaces/ITornadoInstance.sol";
+import "./PGRouter.sol";
+
+contract InstanceRegistry {
+    using SafeERC20 for IERC20;
+
+    enum InstanceState {
+        DISABLED,
+        ENABLED,
+        DEPOSIT_DISABLED,
+        WITHDRAW_DISABLED
+    }
+
+    struct Instance {
+        bool isERC20;
+        IERC20 token;
+        InstanceState state;
+        // the fee of the uniswap pool which will be used to get a TWAP
+        uint24 uniswapPoolSwappingFee;
+        // the fee the protocol takes from relayer, it should be multiplied by PROTOCOL_FEE_DIVIDER from FeeManager.sol
+        uint32 protocolFeePercentage;
+    }
+
+    struct TornadoConfig {
+        ITornadoInstance addr;
+        Instance instance;
+    }
+
+    address public immutable governance;
+    PGRouter public router;
+
+    mapping(ITornadoInstance => Instance) public instances;
+    ITornadoInstance[] public instanceIds;
+
+    event InstanceStateUpdated(
+        ITornadoInstance indexed instance,
+        InstanceState state
+    );
+    event RouterRegistered(address pgRouter);
+
+    modifier onlyGovernance() {
+        require(msg.sender == governance, "Not authorized");
+        _;
+    }
+
+    constructor(address _pgRouter, TornadoConfig[] memory _instances) {
+        governance = msg.sender;
+        router = PGRouter(_pgRouter);
+        for (uint256 i = 0; i < _instances.length; i++) {
+            _updateInstance(_instances[i]);
+            instanceIds.push(_instances[i].addr);
+        }
+    }
+
+    /**
+     * @dev Add or update an instance.
+     */
+    function updateInstanceState(ITornadoInstance _addr, InstanceState _state) external virtual onlyGovernance {
+        Instance storage _instance = instances[_addr];
+        _instance.state = _state;
+        emit InstanceStateUpdated(_addr, _state);
+    }
+
+    /**
+     * @dev Add or update an instance.
+     */
+    function updateInstance(
+        TornadoConfig calldata _tornadoConf
+    ) external virtual onlyGovernance {
+        require(
+            _tornadoConf.instance.state != InstanceState.DISABLED,
+            "Use removeInstance() for remove"
+        );
+        if (instances[_tornadoConf.addr].state == InstanceState.DISABLED) {
+            instanceIds.push(_tornadoConf.addr);
+        }
+        _updateInstance(_tornadoConf);
+    }
+
+    /**
+     * @dev Remove an instance.
+     * @param _instanceId The instance id in `instanceIds` mapping to remove.
+     */
+    function removeInstance(
+        uint256 _instanceId
+    ) external virtual onlyGovernance {
+        ITornadoInstance _instance = instanceIds[_instanceId];
+        (bool isERC20, IERC20 token) = (
+            instances[_instance].isERC20,
+            instances[_instance].token
+        );
+
+        if (isERC20) {
+            uint256 allowance = token.allowance(
+                address(router),
+                address(_instance)
+            );
+            if (allowance != 0) {
+                router.approveExactToken(token, address(_instance), 0);
+            }
+        }
+
+        delete instances[_instance];
+        instanceIds[_instanceId] = instanceIds[instanceIds.length - 1];
+        instanceIds.pop();
+        emit InstanceStateUpdated(_instance, InstanceState.DISABLED);
+    }
+
+    /**
+     * @notice This function should allow governance to set a new protocol fee for relayers
+     * @param instance the to update
+     * @param newFee the new fee to use
+     * */
+    function setProtocolFee(
+        ITornadoInstance instance,
+        uint32 newFee
+    ) external onlyGovernance {
+        instances[instance].protocolFeePercentage = newFee;
+    }
+
+    /**
+     * @notice This function should allow governance to set a new tornado proxy address
+     * @param routerAddress address of the new proxy
+     * */
+    function setPGRouter(address routerAddress) external onlyGovernance {
+        router = PGRouter(routerAddress);
+        emit RouterRegistered(routerAddress);
+    }
+
+    function _updateInstance(TornadoConfig memory _tornadoConf) internal virtual {
+        instances[_tornadoConf.addr] = _tornadoConf.instance;
+        if (_tornadoConf.instance.isERC20) {
+            IERC20 token = IERC20(_tornadoConf.addr.token());
+            require(token == _tornadoConf.instance.token, "Incorrect token");
+            uint256 allowance = token.allowance(
+                address(router),
+                address(_tornadoConf.addr)
+            );
+
+            if (allowance == 0) {
+                router.approveExactToken(
+                    token,
+                    address(_tornadoConf.addr),
+                    type(uint256).max
+                );
+            }
+        }
+        emit InstanceStateUpdated(_tornadoConf.addr, _tornadoConf.instance.state);
+    }
+
+    /**
+     * @dev Returns all instance configs
+     */
+    function getAllInstances() public view returns (TornadoConfig[] memory result) {
+        result = new TornadoConfig[](instanceIds.length);
+        for (uint256 i = 0; i < instanceIds.length; i++) {
+            ITornadoInstance _instance = instanceIds[i];
+            result[i] = TornadoConfig({
+                addr: _instance,
+                instance: instances[_instance]
+            });
+        }
+    }
+
+    /**
+     * @dev Returns all instance addresses
+     */
+    function getAllInstanceAddresses()
+        public
+        view
+        returns (ITornadoInstance[] memory result)
+    {
+        result = new ITornadoInstance[](instanceIds.length);
+        for (uint256 i = 0; i < instanceIds.length; i++) {
+            result[i] = instanceIds[i];
+        }
+    }
+
+    /// @notice get erc20 tornado instance token
+    /// @param instance the interface (contract) key to the instance data
+    function getPoolToken(
+        ITornadoInstance instance
+    ) external view returns (address) {
+        return address(instances[instance].token);
+    }
+}
--- contracts/portalgate/PGRouter.sol
+++ contracts/portalgate/PGRouter.sol
@@ -2,18 +2,77 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
 import "./RelayerRegistry.sol";
+import "./InstanceRegistry.sol";
+import "../interfaces/ITornadoInstance.sol";
 import "../tornado-core/Tornado.sol";
 
 contract PGRouter {
-    RelayerRegistry public relayerRegistry;
+    using SafeERC20 for IERC20;
 
-    constructor(address _relayerRegistry) {
+    event EncryptedNote(address indexed sender, bytes encryptedNote);
+
+    address public immutable governance;
+    InstanceRegistry public immutable instanceRegistry;
+    RelayerRegistry public immutable relayerRegistry;
+
+    modifier onlyGovernance() {
+        require(msg.sender == governance, "Not authorized");
+        _;
+    }
+
+    modifier onlyInstanceRegistry() {
+        require(msg.sender == address(instanceRegistry), "Not authorized");
+        _;
+    }
+
+    constructor(
+        address _governance,
+        address _instanceRegistry,
+        address _relayerRegistry
+    ) {
+        governance = _governance;
+        instanceRegistry = InstanceRegistry(_instanceRegistry);
         relayerRegistry = RelayerRegistry(_relayerRegistry);
     }
 
+    function deposit(
+        ITornadoInstance _tornado,
+        bytes32 _commitment,
+        bytes calldata _encryptedNote
+    ) public payable virtual {
+        (
+            bool isERC20,
+            IERC20 token,
+            InstanceRegistry.InstanceState state,
+            ,
+
+        ) = instanceRegistry.instances(_tornado);
+        require(
+            state != InstanceRegistry.InstanceState.DISABLED,
+            "The instance is not supported"
+        );
+        require(
+            state != InstanceRegistry.InstanceState.DEPOSIT_DISABLED,
+            "The instance is not allowed to deposit"
+        );
+
+        if (isERC20) {
+            token.safeTransferFrom(
+                msg.sender,
+                address(this),
+                _tornado.denomination()
+            );
+        }
+        _tornado.deposit{value: msg.value}(_commitment);
+        emit EncryptedNote(msg.sender, _encryptedNote);
+    }
+
     function withdraw(
-        Tornado _tornado,
+        ITornadoInstance _tornado,
         bytes calldata _proof,
         bytes32 _root,
         bytes32 _nullifierHash,
@@ -22,10 +81,20 @@ contract PGRouter {
         uint256 _fee,
         uint256 _refund
     ) public payable virtual {
+        (, , InstanceRegistry.InstanceState state, , ) = instanceRegistry
+            .instances(_tornado);
         require(
-            relayerRegistry.isRelayerRegistered(msg.sender),
-            "Invalid Relayer"
+            state != InstanceRegistry.InstanceState.DISABLED,
+            "The instance is not supported"
         );
+        require(
+            state != InstanceRegistry.InstanceState.WITHDRAW_DISABLED,
+            "The instance is not allowed to withdraw"
+        );
+        require(
+            relayerRegistry.isRelayerRegistered(_relayer),
+            "Invalid Relayer"
+        );        
 
         _tornado.withdraw{value: msg.value}(
             _proof,
@@ -36,5 +105,47 @@ contract PGRouter {
             _fee,
             _refund
         );
+    }
+
+    /**
+     * @dev Sets `amount` allowance of `_spender` over the router's (this contract) tokens.
+     */
+    function approveExactToken(
+        IERC20 _token,
+        address _spender,
+        uint256 _amount
+    ) external onlyInstanceRegistry {
+        _token.safeApprove(_spender, _amount);
+    }
+
+    /**
+     * @notice Manually backup encrypted notes
+     */
+    function backupNotes(bytes[] calldata _encryptedNotes) external virtual {
+        for (uint256 i = 0; i < _encryptedNotes.length; i++) {
+            emit EncryptedNote(msg.sender, _encryptedNotes[i]);
+        }
+    }
+
+    /// @dev Method to claim junk and accidentally sent tokens
+    function rescueTokens(
+        IERC20 _token,
+        address payable _to,
+        uint256 _amount
+    ) external virtual onlyGovernance {
+        require(_to != address(0), "PG: can not send to zero address");
+
+        if (address(_token) == address(0)) {
+            // for Ether
+            uint256 totalBalance = address(this).balance;
+            uint256 balance = Math.min(totalBalance, _amount);
+            _to.transfer(balance);
+        } else {
+            // any other erc20
+            uint256 totalBalance = _token.balanceOf(address(this));
+            uint256 balance = Math.min(totalBalance, _amount);
+            require(balance > 0, "PG: trying to send 0 balance");
+            _token.safeTransfer(_to, balance);
+        }
     }
 }
